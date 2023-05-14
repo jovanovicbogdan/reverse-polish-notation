3460682d87fdcf2282f1c7f8603048cb169afc40
Bogdan Jovanovic <jbogdan1998@hotmail.com>
2023-05-14 13:10:25 +0200

    Refactor code

--- src/main/java/com/rpn/ReversePolishNotation.java
+++ src/main/java/com/rpn/ReversePolishNotation.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class ReversePolishNotation {
 
-  public static void main(String[] arg) {
+  public static void main(String[] args) {
     String[] tokens = new String[] { "2", "1", "+", "3", "*" };
     System.out.println(evaluateReversePolishNotation(tokens));
   }
@@ -12,42 +12,41 @@ public class ReversePolishNotation {
   public static int evaluateReversePolishNotation(String[] tokens) {
     Stack<Integer> evaluation = new Stack<>();
 
-    int leftOperand = 0;
-    int rightOperand = 0;
     for (String token : tokens) {
-      switch (token) {
-        case "+":
-          evaluation.push(evaluation.pop() + evaluation.pop());
-          break;
-        case "-":
-          rightOperand = evaluation.pop();
-          leftOperand = evaluation.pop();
-          evaluation.push(leftOperand - rightOperand);
-          break;
-        case "*":
-          evaluation.push(evaluation.pop() * evaluation.pop());
-          break;
-        case "/":
-          rightOperand = evaluation.pop();
-          leftOperand = evaluation.pop();
-          evaluation.push(leftOperand / rightOperand);
-          break;
-        default:
-          evaluation.push(Integer.parseInt(token));
-          break;
+      if (isOperator(token)) {
+        int rightOperand = evaluation.pop();
+        int leftOperand = evaluation.pop();
+        int result = performOperation(token, leftOperand, rightOperand);
+        evaluation.push(result);
+      } else {
+        int operand = Integer.parseInt(token);
+        evaluation.push(operand);
       }
     }
 
     return evaluation.pop();
   }
 
-  public static boolean isNumber(String str) {
-    try {
-      Double.parseDouble(str);
-      return true;
-    } catch (NumberFormatException ex) {
-      return false;
+  public static int performOperation(String operator, int leftOperand, int rightOperand) {
+    switch (operator) {
+      case "+":
+        return leftOperand + rightOperand;
+      case "-":
+        return leftOperand - rightOperand;
+      case "*":
+        return leftOperand * rightOperand;
+      case "/":
+        if (rightOperand == 0) {
+          throw new ArithmeticException("Division by zero");
+        }
+        return leftOperand / rightOperand;
+      default:
+        throw new IllegalArgumentException("Invalid operator " + operator);
     }
+  }
+
+  public static boolean isOperator(String token) {
+    return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
   }
 
 }
